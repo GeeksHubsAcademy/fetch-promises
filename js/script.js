8b42e6a63516ff6aec298c72bbb222077db990bb
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-09-24 17:07:55 +0200

    apply single responsibility principle over the getCharacters function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const spinner = document.querySelector('.spinner');
+
+const renderCharacters = (characters = []) => {
+    characters.forEach(character => {
+        document.querySelector('.characters').innerHTML += `
+    <div class="character">
+    <h3>${character.name}</h3>
+    <img src="${character.image}" alt="${character.name}"/>
+    </div>
+    `
+    })
+}
+
 const getCharacters = () => {
     spinner.style.display = 'block';
     fetch('https://rickandmortyapi.com/api/character')
         .then(res => res.json())
         .then(res => {
             // const characters = res.results;
-            const {
-                results: characters
-            } = res;
-            characters.forEach(character => {
-                document.querySelector('.characters').innerHTML += `
-            <div class="character">
-            <h3>${character.name}</h3>
-            <img src="${character.image}" alt="${character.name}"/>
-            </div>
-            `
-            })
+            const { results: characters } = res;
+            renderCharacters(characters);
         })
         .catch(error => console.error(error))
         .finally(() => spinner.style.display = 'none');
