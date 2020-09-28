ff112533f6350f3a70cd8e53575a2c83566d808a
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-09-28 12:55:09 +0200

    add filter search input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let characters = [];
 const spinner = document.querySelector('.spinner');
 const charactersSelected1Div = document.querySelector('.charactersSelected1');
 const charactersSelected2Div = document.querySelector('.charactersSelected2');
@@ -17,8 +18,9 @@ const selectCharacter = (characterId) => {
 }
 
 const renderCharacters = (characters = []) => {
+    charactersDiv.innerHTML = '';
     characters.forEach(character => {
-        document.querySelector('.characters').innerHTML += `
+        charactersDiv.innerHTML += `
     <div class="character" 
     onclick="selectCharacter(${character.id})"
      id="${character.id}"
@@ -37,13 +39,17 @@ const getCharacters = async() => {
         // let res = await fetch('characters.json');
         res = await res.json();
         // const characters = res.results;
-        const {
-            results: characters
-        } = res;
+        characters = res.results;
         renderCharacters(characters);
     } catch (error) {
         console.error(error)
     } finally {
         spinner.style.display = 'none'
     }
+}
+const searchCharacter = event => {
+    const search = event.target.value.toLowerCase()
+    console.log(event.target, search);
+    const charactersFiltered = characters.filter(character => character.name.toLowerCase().includes(search))
+    renderCharacters(charactersFiltered)
 }
